18dfc0c1ee9edbcb21faaf5f1428fa6afcd9e9cd
Singularity0 <mr.mihail.petrov@gmail.com>
2015-12-03 20:26:40 +0200

    bank account solution

---
+++ week3/BankAccount/bank_account.py
@@ -0,0 +1,69 @@
+class BankAccount:
+    def __init__(self, name, bal, currency):
+        self._balance = bal
+        self.name = name
+        self.currency = currency
+        self._history = ["Account was created"]
+
+    def deposit(self, amount):
+        self._balance += amount
+        self._history.append("Deposited {}$".format(amount))
+        return self._balance
+
+    def balance(self):
+        self._history.append("Balance checked ->  {}$".format(self._balance))
+        return self._balance
+
+    def withdraw(self, amount):
+        self._history.append("{}$ was withdrawed".format(amount))
+        amount = amount
+        if self._balance >= amount:
+            self._balance -= amount
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        message = "Bank account for {} with balance of {}{}".format(self.name, self._balance, self.currency)
+        return message
+
+    def __int__(self):
+        self._history.append("__int__ check -> {}$".format(self._balance))
+        return self._balance
+
+    def transfer_to(self, account, amount):
+        if self._balance >= amount:
+            if self.currency == account.currency:
+                self._balance -= amount
+                self._history.append("The amount of {} transferred to the {} account".format(amount, account.name))
+                account._history.append("Recevied {} from source {}".format(amount, self.name))
+                account._balance += amount
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        return 1000
+
+    def history(self):
+        self._history.append("History of {} account called".format(self.name))
+        return self._history
+
+# account = BankAccount("Rado", 100, "$")
+# print(account)
+# print(account.deposit(13))
+# print(account.balance())
+# print(str(account))
+# print(int(account))
+# account.withdraw(100)
+# print(account.balance())
+# print(account.history())
+
+# rado = BankAccount("Rado", 1000, "BGN")
+# ivo = BankAccount("Ivo", 0, "BGN")
+# print(rado.transfer_to(ivo, 500))
+# print(rado.balance())
+# print(ivo.balance())
+# print(rado.history())
+# print(ivo.history())
